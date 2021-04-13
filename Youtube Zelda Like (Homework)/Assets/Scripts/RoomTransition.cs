e590accc3b860107944a71686d19aeb2d51fcd72
MaDness1184 <dev.ryan1184@gmail.com>
2021-04-12 22:19:23 -0700

    Title Cards

--- Youtube Zelda Like (Homework)/Assets/Scripts/RoomTransition.cs
+++ Youtube Zelda Like (Homework)/Assets/Scripts/RoomTransition.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomTransition : MonoBehaviour
 {
+    [Header ("Player & Camera")]
     public Vector3 playerChange;
     public GameObject transitionCam;
+
+    [Header ("Area Title")]
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
+    public float spawnDelay = 2.6f;
+    public float lifeTime = 4f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,6 +24,19 @@ public class RoomTransition : MonoBehaviour
             collision.transform.position += playerChange;
             FindObjectOfType<Cameras>().ChangePlayerCam(transitionCam);
             transitionCam.SetActive(true);
+            if(needText)
+            {
+                StartCoroutine(AreaTitleCo());
+            }
         }
+    }
+
+    private IEnumerator AreaTitleCo()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(lifeTime);
+        text.SetActive(false);
     }
 }
