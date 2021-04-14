52c9608ce8e0d038a0e367be8bf730335fcf75ba
MaDness1184 <dev.ryan1184@gmail.com>
2021-04-13 23:26:51 -0700

    Some small changes

--- Youtube Zelda Like (Homework)/Assets/Scripts/Dialogue.cs
+++ Youtube Zelda Like (Homework)/Assets/Scripts/Dialogue.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Dialoge")]
 public class Dialogue : ScriptableObject
 {
-    [TextArea(14,10)] [SerializeField] string DialogueText;
+    [TextArea(14,10)] public string DialogueText;
 
     public string GetText()
     {
